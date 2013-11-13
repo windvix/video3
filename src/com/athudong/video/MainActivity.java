677d033cd7c35ad9ce13886c712dfe88eded5516
windvix <windvix@foxmail.com>
2013-11-13 09:41:42 +0800

    2013/11/13 09:41

--- src/com/athudong/video/MainActivity.java
+++ src/com/athudong/video/MainActivity.java
@@ -2,13 +2,17 @@ package com.athudong.video;
 
 import com.athudong.video.task.BaseTask;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
 
 public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void initView(Bundle savedInstanceState) {
-		
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
@@ -31,5 +35,47 @@ public class MainActivity extends BaseActivity {
 		
 	}
 
+	// 重写Activity中onKeyDown（）方法
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {// 当keyCode等于退出事件值时
+			ToQuitTheApp();
+			return false;
+		} else {
+			return super.onKeyDown(keyCode, event);
+		}
+	}
 
+	private boolean isExit = false;
+
+	// 封装ToQuitTheApp方法
+	private void ToQuitTheApp() {
+		if (isExit) {
+			// ACTION_MAIN with category CATEGORY_HOME 启动主屏幕
+			Intent intent = new Intent(Intent.ACTION_MAIN);
+			intent.addCategory(Intent.CATEGORY_HOME);
+			startActivity(intent);
+
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					MainActivity.this.finish();
+					System.exit(0);// 使虚拟机停止运行并退出程序
+				}
+			}, 1500);
+		} else {
+			isExit = true;
+			toast("再按一次退出");
+			mHandler.sendEmptyMessageDelayed(0, 3000);// 3秒后发送消息
+		}
+	}
+
+	// 创建Handler对象，用来处理消息
+	private Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {// 处理消息
+			super.handleMessage(msg);
+			isExit = false;
+		}
+	};
 }
